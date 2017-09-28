fb26495bcb2c7b0f8f247d3959c33b1ac095839e
Dominic-Pace <dpace@nhpace.net>
2017-09-28 00:03:04 -0400

    Basic project structure setup.

---
+++ src/core/display-page/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class DisplayPage extends Component {
+  render() {
+    return (
+      <h1>Hi</h1>
+    )
+  }
+}
+
+export default DisplayPage;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+
+import reducer from './reducer'
+import App from './core/display-page'
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore)
+const store = createStoreWithMiddleware(reducer)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'))
+
 registerServiceWorker();
